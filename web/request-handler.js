87c05f7731bf948090c079ecc12b6569058d0ffd
Jawadban <jawadbanga@gmail.com>
2017-01-12 00:35:54 -0800

    Working on parsing response

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,20 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var url = require('url');
+var fs = require('fs');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  if (req.method === 'GET'){
+	if (req.url === '/'){
+	  res.writeHead(200, {'content-type': 'HTML'});
+	  fs.createReadStream(archive.paths.siteAssets + './index.html').pipe(res);
+	  res.end();
+	}
+  } else {
+	res.writeHead(404, {'content-type': 'text'});
+	res.write('Error 404, request not found');
+	res.end();
+  }
+  //res.end(archive.paths.list);
 };
